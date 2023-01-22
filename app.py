d58241fed0759045190c36d09e53fd07cbc56ade
Marcus Bergo <eu@mbergo.dev>
2023-01-21 21:37:29 -0300

    Setting Temperature and Size

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import openai
+import jsonify
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def index():
     if request.method == "POST":
         user_input = request.form["text"]
         # Use the user_input to generate marketing text using a language model
-        api_key = "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"
+        
         model_engine = "text-davinci-002"
         prompt = (f"complete the text: {user_input}")
         completions = openai.Completion.create(
@@ -18,10 +19,10 @@ def index():
             n=1,
             stop=None,
             temperature=0.7,
-            api_key=api_key
+            api_key="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"
         )
         message = completions.choices[0].text
-        return render_template("index.html", message=message)
+        return jsonify({"message": message})
     return render_template("index.html")
 
 if __name__ == "__main__":
