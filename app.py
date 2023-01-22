3613f7cf81a1d3dbf97684d5e24790ff28028ad2
Marcus Bergo <eu@mbergo.dev>
2023-01-21 23:19:44 -0300

    Refactors

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import openai
-import jsonify
 
 app = Flask(__name__)
 
@@ -9,20 +8,19 @@ def index():
     if request.method == "POST":
         user_input = request.form["text"]
         # Use the user_input to generate marketing text using a language model
-        api_key = "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"
         model_engine = "text-davinci-002"
         prompt = (f"complete the text: {user_input}")
         completions = openai.Completion.create(
             engine=model_engine,
             prompt=prompt,
-            max_tokens=1024,
+            max_tokens=int(request.form["size"]),
             n=1,
             stop=None,
-            temperature=0.7,
-            api_key="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"
+            temperature=float(request.form["temp"]),
+            api_key = "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"
         )
         message = completions.choices[0].text
-        return jsonify({"message": message})
+        return render_template("index.html", message=message)
     return render_template("index.html")
 
 if __name__ == "__main__":
