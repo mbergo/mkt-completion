fb45c75704efcbea49ec5b1c64c76df0e9546e02
Marcus Bergo <eu@mbergo.dev>
2023-01-21 23:35:22 -0300

    Trying implement translate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import openai
+from googletrans import Translator
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ def index():
         # Use the user_input to generate marketing text using a language model
         model_engine = "text-davinci-002"
         prompt = (f"complete the text: {user_input}")
+        Translator.translate(user_input, dest='en').text
         completions = openai.Completion.create(
             engine=model_engine,
             prompt=prompt,
@@ -17,9 +19,10 @@ def index():
             n=1,
             stop=None,
             temperature=float(request.form["temp"]),
-            api_key = "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"
+            api_key = "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"
         )
         message = completions.choices[0].text
+        message = Translator.translate(message, dest=request.form["lang"]).text
         return render_template("index.html", message=message)
     return render_template("index.html")
 
